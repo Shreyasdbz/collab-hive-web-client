f9e2f6a4f13708ae7fa2757ae2e505c04c955576
Shreyas Sane <shreyasdbz@gmail.com>
2025-01-09 15:11:09 -0500

    bug

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,5 +1,11 @@
 import PageWrapper from "@/components/layout/page-wrapper";
 
+// FIXME: Next.js has a bug which does not resolve not-found.page.tsx correctly
+// Instead, use `not-found.ts` as a workaround
+// "ts" is required to resolve `not-found.ts`
+// https://github.com/vercel/next.js/issues/65447
+export const dynamic = "force-dynamic";
+
 export default function Custom404() {
   return (
     <PageWrapper title="Oops!">
